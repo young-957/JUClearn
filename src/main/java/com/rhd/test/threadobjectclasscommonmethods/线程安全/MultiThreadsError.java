deabb55061a0d1534c125cfe304b66ab2a2e6aa8
young-957 <1806198497@qq.com>
2023-03-08 10:13:55 +0800

    线程安全

--- src/main/java/com/rhd/test/threadobjectclasscommonmethods/线程安全/MultiThreadsError.java
+++ src/main/java/com/rhd/test/threadobjectclasscommonmethods/线程安全/MultiThreadsError.java
@@ -11,9 +11,22 @@ package com.rhd.test.threadobjectclasscommonmethods.线程安全;
  */
 public class MultiThreadsError implements Runnable{
 
+    public static void main(String[] args) throws InterruptedException{
+        MultiThreadsError multiThreadsError = new MultiThreadsError();
+        Thread thread1 = new Thread(multiThreadsError);
+        Thread thread2 = new Thread(multiThreadsError);
+        thread1.start();
+        thread2.start();
+        thread1.join();
+        thread2.join();
+        System.out.println("表面上结果是： " + multiThreadsError.index);
+    }
 
+    static int index = 0;
     @Override
     public void run() {
-
+        for (int i = 0; i < 10000; i++){
+            index++;
+        }
     }
 }
